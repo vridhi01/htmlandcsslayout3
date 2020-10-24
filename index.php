de905dae47eb8a40e76971e7836ce1052e0e8a51
deepak <deepak@excellencetechnologies.in>
2020-10-24 11:25:05 +0530

    layout3

---
+++ index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <title>Layout3</title>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="index.css">
+      <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+   </head>
+   <body>
+      <div class="navigation">
+         <div class="stylenavigation">
+            <li><a class="navigationlink1" href="#">Layout 3</a></li>
+            <li><a class="navigationlink2" href="#">About</a></li>
+            <li><a class="navigationlink3" href="#">Services</a></li>
+            <li><a class="navigationlink4" href="#">Contact</a></li>
+         </div>
+      </div>
+      <div class="middlediv">
+         <div class="innerdiv">
+            <!--  left side div -->
+            <div class="leftsidediv">
+                <!-- // first div start -->
+               <div class="firstdivls">
+                  <h2 class="firstdivhlstyle">Blog Post Title </h2>
+                  <p class="firstdivpsltyle"> by <span class="spanstyle">Test</span> 
+                  <p>
+               </div>
+                 <!--  first div finish -->
+               <!-- // second div start -->
+               <div class="seconddivls">
+                   <p class="seconddivpsltyle"><i class="far fa-clock"></i>Posted on August 24,2013 at 9:00 PM.<p>
+                </div>
+                <!-- second div finish -->
+                <!-- Third div start -->
+                <div class="thirddivls">
+                    <img src="image.jpg" class="imagel"/>
+                </div>
+                <!-- Third div finish -->
+                <!-- Fourth div start -->
+                <div class="fourthdivls">
+                   <p class="thirddivpsltyle1">
+                       Lorum ipsum dolor sit amet ,consecturer adipiscing elit . Ducimus,vero,<br/>
+                       obcaecati,aut,error quam sapiente nemo saepe quibusdam sit
+                       expecturi nam quia corporis eligendi eos magni recusande laborum minus inventor? <br/>
+                  </p>
+                   <p class="thirddivpsltyle2">
+                      
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                       Ut enim ad minim veniam, quis nostrud exercitation ullamco.  <br/><br/>
+                             
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                       Ut enim ad minim veniam, quis nostrud exercitation ullamco.<br/><br/>
+
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                       Ut enim ad minim veniam, quis nostrud exercitation ullamco.
+                      
+                   </p>
+               </div>
+                <!-- Fourth div finish -->
+                <div class="footer">
+                  <div class="copyright">
+                     <p class="copyrighttext">Copyright © Your Website 2014</p>
+                  </div>
+               </div>
+            </div>
+
+             <!-- // right side div -->
+             <div class="rightsidediv">
+            <!-- first div start -->
+             <div class="firstdivrs">
+                <div class="middlersdiv1">
+                <h2 class="firstdivhrstyle">Blog Search</h2>
+                  <div class="inputsearch">
+                  <input type="search" class="stylesearch" /> </div>
+                 
+                  <button type="button" class="iconsearch">
+                  <i class="fas fa-search"></i>
+                 </button>
+               </div>
+             </div>
+            <!-- first div finish -->
+            <!-- second div start -->
+            <div class="seconddivrs">
+               <div class="middlersdiv2">
+                <h2 class="seconddivhrstyle">Blog Categories</h2>
+                  <div class="category1">
+                     <p>Category Name</p>
+                     <p>Category Name</p>
+                     <p>Category Name</p>
+                     <p>Category Name</p></div>
+                  <div class="category2">
+                     <p>Category Name</p>
+                     <p>Category Name</p>
+                     <p>Category Name</p>
+                     <p>Category Name</p></div>
+               </div>
+             </div>
+             <!-- second div finish -->
+             <!-- third div start -->
+             <div class="thirddivrs">
+                <div class="middlersdiv3">
+                   <h2 class="thirddivhrstyle"> Side Widget Well </h2>
+                 <p class="thirdtdivprstyle">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed ipsum et justo efficitur auctor. Nulla augue ipsum, 
+                    viverra quis enim eget.</p>
+               </div>
+             </div>
+             <!-- third div finish -->
+             </div>
+         </div>
+      </div>
+    </body>
+</html>
